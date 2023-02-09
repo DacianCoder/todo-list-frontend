12065f51df9f7c86fd7c816f8972ec5ab14afa66
Cristian Boza <cristian.boza@pitech.webhelp.com>
2023-02-09 10:35:12 +0200

    Blank start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import {useEffect} from 'react'
-import {FaSave, FaTrash} from 'react-icons/all';
+import React from 'react'
 
 function App() {
 
-
-    useEffect(() => {
-        fetch('http://localhost:8090/todo-list', {
-            method: 'GET',
-        })
-            .then(response => response.json())
-            .then(result => result.items)
-    }, [])
-
     return (
         <div className="app">
-            <h1>
-                My todo list
-            </h1>
-            <div className="items">
-                {['abc', 'Testsdasda sda sdasdas  asd asdasdasdasddas das das das das ', 'a', 't', 'l', 'f'].map(el => <div
-                    className="item">{el} <FaTrash className="deleteIcon"/></div>)}
-            </div>
-            <div className="newTodo">
-                <div>
-                    <label htmlFor="newTodo">Insert new todo: </label>
-                    <input id="newTodo" type="text"/>
-                </div>
-                <FaSave className="saveIcon"/>
-            </div>
+
         </div>
     )
 }
