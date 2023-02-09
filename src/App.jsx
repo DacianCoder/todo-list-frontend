76cca94c9608df9e9acebf9019adb9cb6b9f0932
Cristian Boza <cristian.boza@pitech.webhelp.com>
2023-02-09 10:37:49 +0200

    List todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        loadTodoList();
+    }, [])
+
+    function loadTodoList() {
+        fetch("http://localhost:8080/todo-list")
+            .then(r => r.json())
+            .then(r => setItems(r.items))
+    }
 
     return (
         <div className="app">
-
+            <h1>My todo list</h1>
+            <div className="items">
+                {
+                    items.map(item => <div className={"item"}>{item}</div>)
+                }
+            </div>
         </div>
     )
 }
