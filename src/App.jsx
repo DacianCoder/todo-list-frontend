b7ced033a36988545d671d956f73030d8d6aa43b
Cristian Boza <cristian.boza@pitech.webhelp.com>
2023-02-09 10:40:47 +0200

    Add new todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react'
+import {FaSave} from "react-icons/all.js";
 
 function App() {
     const [items, setItems] = useState([])
+    const [newTodo, setNewTodo] = useState('')
 
     useEffect(() => {
         loadTodoList();
@@ -13,13 +15,29 @@ function App() {
             .then(r => setItems(r.items))
     }
 
+    function saveNewTodo() {
+        const body = JSON.stringify({items: [...items, newTodo]});
+        fetch("http://localhost:8080/todo-list", {method: 'POST', body:body})
+            .then(() => {
+                setNewTodo("");
+                loadTodoList();
+            })
+
+    }
+
     return (
         <div className="app">
             <h1>My todo list</h1>
             <div className="items">
                 {
-                    items.map(item => <div className={"item"}>{item}</div>)
+                    items.map(item => <div className={"item"}>{item}
+                    </div>)
                 }
+            </div>
+            <div className="newTodo">
+                <label htmlFor="newTodo">Insert new todo: </label>
+                <input type="text" id="newTodo" value={newTodo} onChange={ev => setNewTodo(ev.target.value)}/>
+                <FaSave className="saveIcon" onClick={saveNewTodo}/>
             </div>
         </div>
     )
