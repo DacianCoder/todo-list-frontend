45abf2904d1913aa0494ea8bbd41f296bc55ede6
Cristian Boza <cristian.boza@pitech.webhelp.com>
2023-02-09 10:41:59 +0200

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {FaSave} from "react-icons/all.js";
+import {FaSave, FaTrash} from "react-icons/all.js";
 
 function App() {
     const [items, setItems] = useState([])
@@ -17,7 +17,7 @@ function App() {
 
     function saveNewTodo() {
         const body = JSON.stringify({items: [...items, newTodo]});
-        fetch("http://localhost:8080/todo-list", {method: 'POST', body:body})
+        fetch("http://localhost:8080/todo-list", {method: 'POST', body: body})
             .then(() => {
                 setNewTodo("");
                 loadTodoList();
@@ -25,22 +25,25 @@ function App() {
 
     }
 
-    return (
-        <div className="app">
+    function deleteTodo(todoToBeDelete) {
+        const body = JSON.stringify({items: items.filter(item => todoToBeDelete !== item)});
+        fetch("http://localhost:8080/todo-list", {method: 'POST', body: body})
+            .then(() => loadTodoList())
+    }
+
+    return (<div className="app">
             <h1>My todo list</h1>
             <div className="items">
-                {
-                    items.map(item => <div className={"item"}>{item}
-                    </div>)
-                }
+                {items.map(item => <div className={"item"}>{item}
+                    <FaTrash className="deleteIcon" onClick={() => deleteTodo(item)}/>
+                </div>)}
             </div>
             <div className="newTodo">
                 <label htmlFor="newTodo">Insert new todo: </label>
                 <input type="text" id="newTodo" value={newTodo} onChange={ev => setNewTodo(ev.target.value)}/>
                 <FaSave className="saveIcon" onClick={saveNewTodo}/>
             </div>
-        </div>
-    )
+        </div>)
 }
 
 export default App
